b0f050a59653da3bde100ee8990632f3cc571a29
流年空人心 <791745839@qq.com>
2017-01-05 06:15:35 -0800

    20170105 study

--- STL/string/test.cpp
+++ STL/string/test.cpp
@@ -8,49 +8,17 @@
 #include<iostream>
 using namespace std;
 #include<string>
-template <typename T>
-T fun( T a, T b )
-{
-    T c;
-	try( c=a/b )
-		if( b==0 )
-			throw "chushu can not be 0!";
-	return c;
-}
-void Creat_objects()
-{
-	string s1(10,'c');
-	cout<<s1<<endl;
-	string s2( s1 );
-	cout<<s2<<endl;
-	string s3( "ddddddddddddddddd");
-	cout<<s3<<endl;
-
-	string s4 = "abccccddfjdkfdjkfl";
-	cout<<s4<<endl;
-
-}
-void Property_Of_String( string& str )
-{
-	cout<<str.capacity();
-}
+#include<vector>
 int main()
 {
-//	Creat_objects();
-	string   str1("addddddbcccc");
-	Property_Of_String( str1 );
-	float  a,b,c;
-	while( 1 )
-	{
-    cout<<"input some number"<<endl;
-	cin>>a>>b;
-	c = fun();
-    catch( char * e )
-	{
-		cout<<e<<endl;
-		cout<<"input error!";
-	}
-	}
+	vector<char>v1 = {'a','b','c','d'};
+	char  a[]="adfbc";
+	string s2 ( a, 3 );
+	vector<char>::iterator it;
+	cout<<s2<<endl;
+	string s3( v1.begin(),v1.end());
+	cout<<s3<<endl;
+
 
 
 	return 0;
